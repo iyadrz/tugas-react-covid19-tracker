9c18ee9362fe49b12e85175ebf1ec55130c0a696
Iyad Ramadhan Zulfikar <iyadzulfikar@gmail.com>
2021-04-21 13:18:43 +0800

    Updates

---
+++ precache-manifest.49574971cd7faf3f08bc0f3c8df40b69.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "828d2b71bbc8853e5d6e1347612d765c",
+    "url": "/tugas-react-covid19-tracker/index.html"
+  },
+  {
+    "revision": "9b2f9c4444af1d999a9b",
+    "url": "/tugas-react-covid19-tracker/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "792ba43c9e09d881e25b",
+    "url": "/tugas-react-covid19-tracker/static/css/main.e1f4fc00.chunk.css"
+  },
+  {
+    "revision": "9b2f9c4444af1d999a9b",
+    "url": "/tugas-react-covid19-tracker/static/js/2.ad52a834.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/tugas-react-covid19-tracker/static/js/2.ad52a834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792ba43c9e09d881e25b",
+    "url": "/tugas-react-covid19-tracker/static/js/main.a353e979.chunk.js"
+  },
+  {
+    "revision": "88abe3790da37a8bb867",
+    "url": "/tugas-react-covid19-tracker/static/js/runtime-main.3107c62b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tugas-react-covid19-tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
